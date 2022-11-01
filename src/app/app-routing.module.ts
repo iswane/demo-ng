8bfcb265ab55442f9942f3ae9903f6e05a2f4053
Abdoulaye WANE <laye1wane@gmail.com>
2022-11-01 13:16:52 +0100

    Project initialize

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CompOneComponent } from './comp-one/comp-one.component';
+import { CompTwoComponent } from './comp-two/comp-two.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'comp-one', component: CompOneComponent},
+  {path: 'comp-two', component: CompTwoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
